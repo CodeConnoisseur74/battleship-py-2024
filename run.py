b5b01941f12d60eb9381c57734ccf08a1fffa225
CodeConnoisseur74 <sherry.andrews.com>
2024-05-20 21:37:01 +0200

    create the board cells and initiate game board

--- run.py
+++ run.py
@@ -1,8 +1,12 @@
 # import sys
 # import random
-# from colorama import Fore, Style, init
+from colorama import Fore, Style, init
+
 # import string
 import types
+
+# Initialize colorama
+init()
 
 # Constants
 consts = types.SimpleNamespace()
@@ -27,3 +31,28 @@ def convert_alphabetic_to_numeric(letter: str):
 # Function to convert numeric y-coordinate to alphabetic coordinate
 def convert_numeric_to_alphabetic(numeric: int):
     return chr(ord("A") + numeric)
+
+
+def format_cell(cell: str, show_ships) -> str:
+    match cell:
+        case consts.CHAR_HIT:
+            return Fore.RED + cell + Style.RESET_ALL
+        case consts.CHAR_MISS:
+            return Fore.WHITE + cell + Style.RESET_ALL
+        case consts.CHAR_WATER:
+            return Fore.CYAN + cell + Style.RESET_ALL
+        case _:
+            return (
+                Fore.YELLOW + cell + Style.RESET_ALL
+                if show_ships
+                else Fore.CYAN + consts.CHAR_WATER + Style.RESET_ALL
+            )
+
+
+# Function to initialize an empty game board
+def initialize_board():
+    board = []
+    for _ in range(consts.BOARD_SIZE):
+        row = [consts.CHAR_WATER] * consts.BOARD_SIZE
+        board.append(row)
+    return board
